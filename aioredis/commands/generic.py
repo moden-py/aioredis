f33becf7b70efc45f61757c89364897969deeb56
Alexey Popravka <alexey.popravka@horsedevel.com>
2016-06-03 21:51:16 +0300

    more test fixes

--- aioredis/commands/generic.py
+++ aioredis/commands/generic.py
@@ -82,7 +82,7 @@ class GenericCommandsMixin:
         if replace:
             flags.append(b'REPLACE')
         fut = self.execute(b'MIGRATE', host, port,
-                                         key, dest_db, timeout, *flags)
+                           key, dest_db, timeout, *flags)
         return wait_ok(fut)
 
     def move(self, key, db):
