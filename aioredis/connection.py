75b474d16a0d4d4e9dda2d3fd70f5736bcfb8557
Alexey Popravka <alexey.popravka@horsedevel.com>
2016-06-21 15:30:31 +0300

    drop 'get_atomic_connection'

--- aioredis/connection.py
+++ aioredis/connection.py
@@ -320,6 +320,7 @@ class RedisConnection:
 
     @asyncio.coroutine
     def wait_closed(self):
+        """Coroutine waiting until connection is closed."""
         yield from asyncio.shield(self._close_waiter, loop=self._loop)
 
     @property
@@ -423,7 +424,3 @@ class RedisConnection:
         """Authenticate to server."""
         fut = self.execute('AUTH', password)
         return wait_ok(fut)
-
-    @asyncio.coroutine
-    def get_atomic_connection(self):
-        return self
